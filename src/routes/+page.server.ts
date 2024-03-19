40f98928e16667a34cace05e518235b35c16009d
Sreetam Das <sreetamdas@gmail.com>
2024-03-19 09:37:38 -0400

    Fetch article content on server, parse using `cheerio` and render

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,53 @@
+import type { PageServerLoad } from "./$types";
+import cheerio from "cheerio";
+
+export const load = (async () => {
+	// const parser = new DOMParser();
+	const headers = new Headers();
+	headers.set(
+		"User-Agent",
+		"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+	);
+
+	const parsed_articles_content = await Promise.all(
+		articles.map(async (article) => {
+			const { url } = article;
+
+			const response = await fetch(url, {
+				method: "GET",
+				headers: headers,
+			});
+			console.log(">>", response);
+
+			const page_html = await response.text();
+			const page_doc = cheerio.load(page_html);
+
+			const title = page_doc('meta[property="og:title"]').attr("content");
+			const description = page_doc('meta[property="og:description"]').attr("content");
+			const image = page_doc('meta[property="og:image"]').attr("content");
+
+			return {
+				title,
+				description,
+				image,
+			};
+		}),
+	);
+
+	return { parsed_articles_content };
+}) satisfies PageServerLoad;
+
+const articles = [
+	{
+		url: "https://www.knoxnews.com/story/news/education/2024/03/18/knox-county-schools-announces-half-day-solar-eclipse-2024/73022092007/",
+	},
+	{
+		url: "https://www.knoxnews.com/story/news/education/2024/03/08/farragut-high-school-students-can-soon-take-offsite-christian-class/72881639007/",
+	},
+	{
+		url: "https://www.knoxnews.com/story/news/education/2024/03/05/knox-county-schools-will-ask-board-to-increase-salaries-in-2024/72844947007/",
+	},
+	{
+		url: "https://www.knoxnews.com/story/money/columnists/cathy-ackermann/2024/03/15/cathy-ackermann-when-is-competition-in-business-good/72965474007/",
+	},
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
+<section class="mx-auto flex max-w-2xl flex-col items-center px-4 py-8">
+	<h1 class="pb-20 pt-20 text-5xl font-bold">Hi! I'm Areena.</h1>
+
+	<div class="">
+		<p class="py-2">
+			I'm a data journalist, occassional food blogger, and 🪴house plant enthusiast!🪴
+		</p>
+		<p class="py-2">
+			I'm new to the world of data-powered storytelling, and slowly paving my path at Knox News.
+		</p>
+		<p class="py-2">
+			I'm also ✨NEW✨ to coding, and learning Svelte. This website is where I showcase my best
+			front-end work.
+		</p>
+	</div>
+</section>
+
+<section class="mx-auto flex max-w-3xl flex-col items-center p-4">
+	<h2 class="py-20 text-3xl font-bold">Articles of Interest</h2>
+
+	<div class="flex gap-4">
+		{#each data.parsed_articles_content as parsed_article}
+			<article>
+				<h3 class="text-lg font-semibold leading-tight">{parsed_article.title}</h3>
+				<p class="pt-4 text-sm italic">{parsed_article.description}</p>
+			</article>
+		{/each}
+	</div>
+</section>
